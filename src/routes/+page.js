26be04f6d0a943084492824da558b3fb941d0e8c
Adrian <adrianthelawx@gmail.com>
2022-10-24 16:29:53 -0600

    Primer commit

---
+++ src/routes/+page.js
@@ -0,0 +1,6 @@
+// import '../assets/test';
+// import '../assets/css/bootstrap.min.css';
+// import '../assets/css/icons.min.css';
+// import '../assets/css/app.min.css';
+// import '../assets/css/custom.min.css';
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { browser } from '$app/environment';
+
+    if(browser){
+        window.localStorage.setItem('user','adrian');
+    }
+</script>
+<a href="/pokemon/2">New pokemon</a>
